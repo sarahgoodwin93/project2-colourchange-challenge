9e4c9875352937c229fa3fd32044fdd36d0f4608
sarahgoodwin93 <>
2023-08-01 16:27:16 +0000

    adjust styles and add first function for click

---
+++ assets/javascript/script.js
@@ -0,0 +1,8 @@
+// Change colour of button fuction
+let one = document.getElementById("one");
+
+function changeBackground() {
+    one.style.backgroundColor = "#ff9e1d";
+}
+
+one.addEventListener('click, changeBackground');
