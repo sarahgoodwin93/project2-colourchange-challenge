87cbb0f3b793be6be9db9be85b239a1cd2e03694
sarahgoodwin93 <>
2023-08-01 18:30:57 +0000

    TEST: test js functions for game buttons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,16 @@
-// Change colour of button fuction
+// Change colour of button fuction - button one
 let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
 
 function changeBackground() {
     one.style.backgroundColor = "#ff9e1d";
+    two.style.backgroundColor = "#ff9e1d";
+    three.style.backgroundColor = "#ff9e1d";
 }
-one.addEventListener("click, changeBackground");
+one.addEventListener("click", changeBackground);
+two.addEventListener("click", changeBackground);
+three.addEventListener("click", changeBackground);
 
-// Change colour back button fuction
-function revertToPink() {
-    one.style.backgroundColor = '#e7014c';
-    one.removeEventListener("click, revertToPink");
-}
 
-one.addEventListener("click, revertToPink");
 
