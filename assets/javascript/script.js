35fd956b1c0a7243582166cd6ab323dd11275ea1
sarahgoodwin93 <>
2023-08-01 18:55:02 +0000

    FEAT: add function to change colour back after clicked

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,14 @@
-// Change colour of button fuction - button one
+// Change colour of buttons on game
+
+let clickedBox = {};
+
 function changeBackground(divId) {
     let div = document.getElementById(divId);
-    div.style.backgroundColor = "#ff9e1d";
+
+    if (clickedBox[divId]) {
+        div.style.backgroundColor = "#e7014c";
+    } else {
+        div.style.backgroundColor = "#ff9e1d";
+        clickedBox[divId] = true;
+    }
 }
-
-
-
