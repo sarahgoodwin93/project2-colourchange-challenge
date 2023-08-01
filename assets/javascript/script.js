a460216285acb35830d225ef3466b0b821c6f232
sarahgoodwin93 <>
2023-08-01 18:48:22 +0000

    FEAT: create function to individually change divs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,16 +1,8 @@
 // Change colour of button fuction - button one
-let one = document.getElementById("one");
-let two = document.getElementById("two");
-let three = document.getElementById("three");
-
-function changeBackground() {
-    one.style.backgroundColor = "#ff9e1d";
-    two.style.backgroundColor = "#ff9e1d";
-    three.style.backgroundColor = "#ff9e1d";
+function changeBackground(divId) {
+    let div = document.getElementById(divId);
+    div.style.backgroundColor = "#ff9e1d";
 }
-one.addEventListener("click", changeBackground);
-two.addEventListener("click", changeBackground);
-three.addEventListener("click", changeBackground);
 
 
 
