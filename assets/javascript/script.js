0cf6cd034d8ef5eda3b1e90e3c51484bd3deea5d
sarahgoodwin93 <>
2023-08-01 17:49:48 +0000

    test js for changing button colours

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,5 +4,13 @@ let one = document.getElementById("one");
 function changeBackground() {
     one.style.backgroundColor = "#ff9e1d";
 }
+one.addEventListener("click, changeBackground");
 
-one.addEventListener('click, changeBackground');
+// Change colour back button fuction
+function revertToPink() {
+    one.style.backgroundColor = '#e7014c';
+    one.removeEventListener("click, revertToPink");
+}
+
+one.addEventListener("click, revertToPink");
+
