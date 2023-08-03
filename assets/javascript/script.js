290a815900d76d1d93ddd57e4cc9381eedf13b8e
sarahgoodwin93 <>
2023-08-03 17:42:57 +0000

    STYLE: remove all js to begin again

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,18 +2,9 @@
 
 let colors = ["#ffb5e8", "#d5aaff", "#6eb5ff", "#fffd1", "#bffcc6", "#ffabab"];
 
-// Change colour of buttons on game
-let clickedBox = {};
+// Change one div to random color
 
-function changeBackground(divId) {
-    let div = document.getElementById(divId);
 
-    if (clickedBox[divId]) {
-        div.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
-    } else {
-        div.style.backgroundColor = "#ff9e1d";
-        clickedBox[divId] = true;
-    }
-}
+
 
 
