8b121d44b8c477dcbba0759bfe5f81a2c7a31078
sarahgoodwin93 <>
2023-08-03 17:49:46 +0000

    FEAT: add functions to change one div color and get random color from array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,29 @@ let colors = ["#ffb5e8", "#d5aaff", "#6eb5ff", "#fffd1", "#bffcc6", "#ffabab"];
 
 // Change one div to random color
 
+function changeBackground(boxId) {
+    if (boxId === changedBox) {
+        clearInterval(intervalId);
+        alert('!');
+    } else {
+        clearInterval(intervalId);
+        alert('!');
+    }
+}
+
+// Get random color from array
+
+function randomColor() {
+    let randomColors = Math.floor(Math.random() * colors.length);
+    return colors[randomColors];
+}
+
+// Make user click on correct div, and win
 
 
+// Reset Game
+
+
+// Start Game
 
 
