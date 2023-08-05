ee5d7a4e8c897be9121b796d3febe6dea1cab88c
sarahgoodwin93 <>
2023-08-05 15:14:24 +0000

    FEAT: add more code comments to js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,16 +9,24 @@ let level = 0;
 
 
 // Colour to use in grid
-
+/**
+ * List of colours that the game can choose from to keep the site design with pastel colours
+ */
 let colors = ["#ffb5e8", "#d5aaff", "#6eb5ff", "#fffd1", "#bffcc6",
     "#ffabab", "#ff9cee", "#ffcf9", "#b28dff", "#c4faf8", "#fff5ba", "#ffbebc"];
 
 // Change one div to random color
 
+/**
+ * This if statement checks if the box that has been clicked matches the box that has changed colour
+ */
 function changeBackground(boxId) {
     if (boxId === changedBox) {
         clearInterval(intervalId);
         startGame();
+        /**
+         * If user clicks on the wrong box the below message will appear
+         */
     } else {
         clearInterval(intervalId);
         alert('Sorry Wrong Box, Try again!');
@@ -26,7 +34,11 @@ function changeBackground(boxId) {
 }
 
 // Get random color from array
-
+/**
+ * 
+ * This function runs through the array of colours outlined in the variable 'colors'
+ * So that they are randomly selected
+ */
 function randomColor() {
     let randomColors = Math.floor(Math.random() * colors.length);
     return colors[randomColors];
@@ -34,27 +46,46 @@ function randomColor() {
 
 // Start Game button
 
+/**
+ * This function begins by making sure the changedBox is resert to null at the beginning of each round
+ */
 function startGame() {
     changedBox = null;
     let allBoxes = document.getElementsByClassName("box");
-
+/**
+ * The for loop goes through each box and makes sure its background is set to the original pink colour
+ */
     for (let i = 0; i < allBoxes.length; i++) {
         allBoxes[i].style.backgroundColor = "#e7014c";
     }
-
+/**
+ * This generates which random box will change colour, and calls the randomColor function written above
+ */
     let randomBox = Math.floor(Math.random() * allBoxes.length);
     changedBox = allBoxes[randomBox].id;
     document.getElementById(changedBox).style.backgroundColor = randomColor();
 
+/**
+ * The clear interval clears any exisiting timer and ensures 
+ * that the previouis internal is stoped before starting a new one.
+ * This was impliments so that the colours did not all change together 
+ */
     clearInterval(intervalId);
     intervalId = setInterval(function () {
         let randomBox = Math.floor(Math.random() * allBoxes.length);
         changedBox = allBoxes[randomBox].id;
         document.getElementById(changedBox).style.backgroundColor = randomColor();
+/**
+ * This time means that the boxes change colour every 2 seconds.
+ */
     }, 2000);
 }
 
 // Start Game Click
+
+/**
+ * This function begins the game once the user clicks on the start game button
+ */
 
 let startButton = document.getElementById("start-game");
 startButton.addEventListener("click", startGame);
@@ -75,6 +106,7 @@ function addLevel() {
     document.getElementById("level").innerText = newLevel;
 }
 
+// Timer
 
 
 
